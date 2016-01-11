6e36a00518ffc8e4e7fe597ef20e6f777254e064
Miles Florence <milesflo15@gmail.com>
2016-01-11 10:31:06 -0800

    Fixed title error && created header partial

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-app.require
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('./pages/index', { title: 'Homepage' });
