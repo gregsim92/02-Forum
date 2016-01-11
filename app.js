00d1212669e6599473551f843127e63d47e44e76
Miles Florence <milesflo15@gmail.com>
2016-01-11 09:49:08 -0800

    added pages, routes, knex, and db  && improved file structure

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ var bodyParser = require('body-parser');
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
-var games = require('./routes/games');
+var subforum = require('./routes/subforum');
 
 var app = express();
 
@@ -25,7 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('/games', games);
+app.use('/:subforum', subforum);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+app.require
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('./pages/index', { title: 'Misc Forum' });
+  res.render('./pages/index', { title: 'Homepage' });
 });
 
 
