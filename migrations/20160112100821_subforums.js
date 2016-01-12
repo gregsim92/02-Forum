ae7b3fd4c972aeab1a6c8a3df7cd58b237f753d1
Miles Florence <milesflo15@gmail.com>
2016-01-12 11:22:19 -0800

    This may bot be necessary

--- migrations/20160112100821_subforums.js
+++ migrations/20160112100821_subforums.js
@@ -3,6 +3,7 @@ exports.up = function(knex, Promise) {
   return knex.schema.createTable('subforums', function(table){
   	table.increments(),
   	table.string('sub_name'),
+  	table.integer('category'),
   	table.text('sub_desc'),
   	table.integer('sub_views'),
   	table.integer('sub_thread_count')
