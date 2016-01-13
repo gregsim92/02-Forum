fff0dbaa60fe5d17bd867a6cbf3ab5121a681a53
Miles Florence <milesflo15@gmail.com>
2016-01-12 16:14:07 -0800

    Threads now append to subforums and redirect properly

--- app.js
+++ app.js
@@ -24,6 +24,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/forums',express.static(path.join(__dirname, 'public')));
+app.use('/users',express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/forums', subforum);
