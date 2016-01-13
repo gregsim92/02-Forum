414550b76ca71df8c78f799018e652c2a7dd6df5
Miles Florence <milesflo15@gmail.com>
2016-01-13 10:21:38 -0800

    Post method added && app.js fixed

--- app.js
+++ app.js
@@ -24,8 +24,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/forums',express.static(path.join(__dirname, 'public')));
-app.use('/users',express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/forums', subforum);
