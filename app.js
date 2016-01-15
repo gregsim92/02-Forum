f58169c6b306d655d9b3e2f745e300dccfcf1bb6
gregsim92 <gregsim92@gmail.com>
2016-01-14 18:13:52 -0800

    fixed dotenv bug

--- app.js
+++ app.js
@@ -22,7 +22,7 @@ app.set('view engine', 'ejs');
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-if(process.env.NODE_ENV){
+if(process.env.NODE_ENV == 'development'){
 
     require('dotenv').load();
   }
